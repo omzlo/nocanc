c23085abe644e2d49e108376c12139920d902374
Alain Pannetrat <L1L1@gmx.com>
2018-05-08 20:58:28 +0300

    Implemented blynk readers, revamped help+config

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,18 +1,90 @@
 package config
 
 import (
+	"fmt"
 	"github.com/BurntSushi/toml"
+	"github.com/omzlo/goblynk"
 	"github.com/omzlo/nocand/models/helpers"
+	"strconv"
+	"strings"
 )
 
+/****/
+
+type BlynkAssoc struct {
+	Pin     uint
+	Channel string
+}
+
+func (ba *BlynkAssoc) Set(s string) error {
+	parts := strings.SplitN(s, "::", 2)
+	pin, err := strconv.ParseUint(parts[0], 0, 32)
+	if err != nil {
+		return err
+	}
+	channel := parts[1]
+	ba.Pin = uint(pin)
+	ba.Channel = channel
+	return nil
+
+}
+
+func (ba *BlynkAssoc) UnmarshalText(text []byte) error {
+	return ba.Set(string(text))
+}
+
+type BlynkMap struct {
+	List []BlynkAssoc
+}
+
+func (bl *BlynkMap) Set(s string) error {
+	bl.List = nil
+
+	v := strings.Split(s, ",")
+	for _, item := range v {
+		var ba BlynkAssoc
+
+		if err := ba.Set(item); err != nil {
+			return err
+		}
+		bl.List = append(bl.List, ba)
+	}
+	return nil
+}
+
+func (bl *BlynkMap) String() string {
+	var s []string
+
+	for _, item := range bl.List {
+		s = append(s, fmt.Sprintf("%i:%s", item.Pin, item.Channel))
+	}
+	return strings.Join(s, ",")
+}
+
+/***/
+
+type BlynkConfiguration struct {
+	BlynkServer string `toml:blynk_server`
+	BlynkToken  string `toml:blynk_token`
+	Readers     BlynkMap
+	Writers     BlynkMap
+}
+
 type Configuration struct {
-	EventServer string
-	AuthToken   string
+	EventServer       string `toml:event_server`
+	AuthToken         string `toml:auth_token`
+	DownloadSizeLimit uint   `toml:download_size_limit`
+	Blynk             BlynkConfiguration
 }
 
 var Settings = Configuration{
-	EventServer: ":4242",
-	AuthToken:   "missing-password",
+	EventServer:       ":4242",
+	AuthToken:         "missing-password",
+	DownloadSizeLimit: (1 << 32) - 1,
+	Blynk: BlynkConfiguration{
+		BlynkServer: blynk.BLYNK_ADDRESS,
+		BlynkToken:  "missing-token",
+	},
 }
 
 func Load() error {
--- cmd/nocanc.go
+++ cmd/nocanc.go
@@ -9,64 +9,49 @@ import (
 	"github.com/omzlo/nocand/models/device"
 	"github.com/omzlo/nocand/models/nocan"
 	"github.com/omzlo/nocand/socket"
-	"io"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 	"time"
 )
 
-type BlynkAssoc struct {
-	Pin     uint
-	Channel string
-}
-
-type BlynkMap struct {
-	List []BlynkAssoc
-}
-
-func (bl *BlynkMap) Set(s string) error {
-	v := strings.Split(s, ",")
-	for _, item := range v {
-		parts := strings.SplitN(item, ":", 2)
-		pin, err := strconv.ParseUint(parts[0], 0, 32)
-		if err != nil {
-			return err
-		}
-		channel := parts[1]
-		bl.List = append(bl.List, BlynkAssoc{uint(pin), channel})
-	}
-	return nil
-}
-
-func (bl *BlynkMap) String() string {
-	var s []string
-
-	for _, item := range bl.List {
-		s = append(s, fmt.Sprintf("%i:%s", item.Pin, item.Channel))
-	}
-	return strings.Join(s, ",")
-}
+/***/
 
 var (
-	OptSizeLimit    uint
-	OptBlynkReaders BlynkMap
-	OptBlynkWriters BlynkMap
+	OptSizeLimit uint
 )
 
-func NewFlagSet(cmd string) *flag.FlagSet {
+func EmptyFlagSet(cmd string) *flag.FlagSet {
+	return flag.NewFlagSet(cmd, flag.ExitOnError)
+}
+
+func BaseFlagSet(cmd string) *flag.FlagSet {
 	fs := flag.NewFlagSet(cmd, flag.ExitOnError)
 	fs.StringVar(&config.Settings.EventServer, "event-server", config.Settings.EventServer, "Address of event server")
 	fs.StringVar(&config.Settings.AuthToken, "auth-token", config.Settings.AuthToken, "Authentication key")
 	return fs
 }
 
-func monitor_cmd(args []string) error {
-	fs := NewFlagSet("monitor")
+func BlynkFlagSet(cmd string) *flag.FlagSet {
+	fs := BaseFlagSet(cmd)
+	fs.StringVar(&config.Settings.Blynk.BlynkServer, "blynk-server", config.Settings.Blynk.BlynkServer, "Address of blynk server")
+	fs.StringVar(&config.Settings.Blynk.BlynkToken, "blynk-token", config.Settings.Blynk.BlynkToken, "Blynk authentication token value")
+	fs.Var(&config.Settings.Blynk.Readers, "readers", "list of reader mappings")
+	fs.Var(&config.Settings.Blynk.Writers, "writers", "list of writer mappings")
+	return fs
+}
 
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
+func DownloadFlagSet(cmd string) *flag.FlagSet {
+	fs := BaseFlagSet(cmd)
+	fs.UintVar(&config.Settings.DownloadSizeLimit, "download-size-limit", config.Settings.DownloadSizeLimit, "Download size limit")
+	return fs
+}
+
+/***/
+
+func monitor_cmd(fs *flag.FlagSet) error {
+
 	sl := socket.NewSubscriptionList()
 
 	if len(fs.Args()) > 0 {
@@ -110,14 +95,9 @@ func monitor_cmd(args []string) error {
 	return nil
 }
 
-func publish_cmd(args []string) error {
-	fs := NewFlagSet("publish")
+func publish_cmd(fs *flag.FlagSet) error {
 
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
-
-	args = fs.Args()
+	args := fs.Args()
 
 	if len(args) != 2 {
 		return fmt.Errorf("publish command has two arguments, %d were provided", len(args))
@@ -134,17 +114,9 @@ func publish_cmd(args []string) error {
 	return conn.Put(socket.ChannelUpdateEvent, socket.NewChannelUpdate(channelName, 0xFFFF, socket.CHANNEL_UPDATED, []byte(channelValue)))
 }
 
-func blynk_cmd(args []string) error {
-	fs := NewFlagSet("blynk")
-
-	fs.Var(&OptBlynkReaders, "readers", "list of reader mappings")
-	fs.Var(&OptBlynkWriters, "writers", "list of writer mappings")
-
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
-
-	args = fs.Args()
+func blynk_cmd(fs *flag.FlagSet) error {
+	var pin_to_channel map[uint]string
+	var name_to_channel map[string]*socket.ChannelUpdate
 
 	conn, err := socket.Dial(config.Settings.EventServer, config.Settings.AuthToken)
 	if err != nil {
@@ -152,9 +124,9 @@ func blynk_cmd(args []string) error {
 	}
 	defer conn.Close()
 
-	client := blynk.NewClient(blynk.BLYNK_ADDRESS, "0fee55cf5dc54ffd843c9478a5421226")
+	client := blynk.NewClient(config.Settings.Blynk.BlynkServer, config.Settings.Blynk.BlynkToken)
 
-	for _, writer := range OptBlynkWriters.List {
+	for _, writer := range config.Settings.Blynk.Writers.List {
 		client.RegisterDeviceWriterFunction(writer.Pin, func(pin uint, body blynk.Body) {
 			val, ok := body.AsString(0)
 			if ok {
@@ -162,16 +134,56 @@ func blynk_cmd(args []string) error {
 			}
 		})
 	}
+	if len(config.Settings.Blynk.Readers.List) > 0 {
+		pin_to_channel = make(map[uint]string)
+		name_to_channel = make(map[string]*socket.ChannelUpdate)
+
+		sl := socket.NewSubscriptionList(socket.ChannelUpdateEvent)
+		if err = conn.Subscribe(sl); err != nil {
+			return err
+		}
+
+		go func() {
+			for {
+				value, err := conn.WaitFor(socket.ChannelUpdateEvent)
+
+				if err != nil {
+					return
+				}
+
+				cu := new(socket.ChannelUpdate)
+
+				if err = cu.UnpackValue(value); err != nil {
+					return
+				}
+
+				name_to_channel[cu.Name] = cu
+			}
+		}()
+
+		for _, reader := range config.Settings.Blynk.Readers.List {
+			pin_to_channel[reader.Pin] = reader.Channel
+
+			if err = conn.Put(socket.ChannelUpdateRequestEvent, socket.NewChannelUpdateRequest(reader.Channel, 0xFFFF)); err != nil {
+				return err
+			}
+
+			client.RegisterDeviceReaderFunction(reader.Pin, func(pin uint, body *blynk.Body) {
+				channel_name := pin_to_channel[pin]
+				if channel_name != "" {
+					cu := name_to_channel[channel_name]
+					if cu != nil {
+						body.PushString(string(cu.Value))
+					}
+				}
+			})
+		}
+	}
 	client.Run()
 	return nil
 }
 
-func list_channels_cmd(args []string) error {
-	fs := NewFlagSet("list-channels")
-
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
+func list_channels_cmd(fs *flag.FlagSet) error {
 
 	conn, err := socket.Dial(config.Settings.EventServer, config.Settings.AuthToken)
 	if err != nil {
@@ -203,12 +215,7 @@ func list_channels_cmd(args []string) error {
 	return nil
 }
 
-func list_nodes_cmd(args []string) error {
-	fs := NewFlagSet("list-nodes")
-
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
+func list_nodes_cmd(fs *flag.FlagSet) error {
 
 	conn, err := socket.Dial(config.Settings.EventServer, config.Settings.AuthToken)
 	if err != nil {
@@ -239,16 +246,10 @@ func list_nodes_cmd(args []string) error {
 	return nil
 }
 
-func read_channel_cmd(args []string) error {
-	fs := NewFlagSet("read-channel")
-
+func read_channel_cmd(fs *flag.FlagSet) error {
 	//fs.BoolVar(&OptOnUpdate, "on-update", false, "wait until channel is updated instead of returning last value immediately")
 
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
-
-	args = fs.Args()
+	args := fs.Args()
 
 	if len(args) != 1 {
 		return fmt.Errorf("read-channel command has one argument, %d were provided", len(args))
@@ -295,12 +296,7 @@ func read_channel_cmd(args []string) error {
 	return nil
 }
 
-func upload_cmd(args []string) error {
-	fs := NewFlagSet("upload")
-
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
+func upload_cmd(fs *flag.FlagSet) error {
 
 	xargs := fs.Args()
 
@@ -388,14 +384,7 @@ func upload_cmd(args []string) error {
 	return nil
 }
 
-func download_cmd(args []string) error {
-	fs := NewFlagSet("download")
-
-	fs.UintVar(&OptSizeLimit, "size-limit", (1<<32)-1, "Download size limit")
-
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
+func download_cmd(fs *flag.FlagSet) error {
 
 	xargs := fs.Args()
 
@@ -487,11 +476,43 @@ func download_cmd(args []string) error {
 	return nil
 }
 
-func reboot_cmd(args []string) error {
+func reboot_cmd(fs *flag.FlagSet) error {
 	panic("Unimplemented")
 	return nil
 }
 
+func help_cmd(fs *flag.FlagSet) error {
+	xargs := fs.Args()
+
+	progname := path.Base(os.Args[0])
+
+	if len(xargs) == 0 {
+		fmt.Printf("Usage:\r\n")
+		for _, c := range Commands {
+			fmt.Printf("%s %s\r\n\t- %s\r\n", progname, c.usage, c.help)
+		}
+	} else {
+		if len(xargs) == 1 {
+			c := FindCommandMatches(xargs[0])
+			if len(c) == 1 {
+				fmt.Printf("%s %s\r\n\t- %s\r\n", progname, c[0].usage, c[0].help)
+				fmt.Printf("This command takes the following options:\r\n")
+				fs2 := c[0].flags(xargs[0])
+				fs2.VisitAll(func(f *flag.Flag) {
+					fmt.Printf("\t-%s\t%s (default %s)\r\n", f.Name, f.Usage, f.DefValue)
+				})
+			} else {
+				fmt.Printf("Ambiguous command. Did you mean:\r\n")
+				for _, v := range c {
+					fmt.Printf("%s help %s\r\n", progname, v.command)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+/*
 var help_text = [...]string{
 	"monitor [options]                          - Monitor all events",
 	"monitor [options] <eid1> <eid2> ...        - Monitor selected event by eid",
@@ -515,11 +536,45 @@ func help(w io.Writer) {
 		fmt.Fprintln(w, "  "+l)
 	}
 }
+*/
+
+type CommandDescriptor struct {
+	command   string
+	processor func(*flag.FlagSet) error
+	flags     func(string) *flag.FlagSet
+	usage     string
+	help      string
+}
+
+var Commands = []*CommandDescriptor{
+	{"blynk", blynk_cmd, BlynkFlagSet, "blynk [options]", "Connect to a blynk server (see https://www.blynk.cc/)"},
+	{"download", download_cmd, DownloadFlagSet, "download [options] <filename> <node_id>", "Download firmware from node"},
+	{"help", nil, EmptyFlagSet, "help <command>", "Provide help about a command, or general help if no command is specified"},
+	{"list-channels", list_channels_cmd, BaseFlagSet, "list-channels [options]", "List all channels"},
+	{"list-nodes", list_nodes_cmd, BaseFlagSet, "list-nodes [options]", "List all nodes"},
+	{"monitor", monitor_cmd, BaseFlagSet, "monitor [options] <eid1> <eid2> ...", "Monitor selected by eid, or all events if no eid specified"},
+	{"publish", publish_cmd, BaseFlagSet, "publish [options] <channel_name> <value>", "Publish <value> to <channel_name>"},
+	{"read-channel", read_channel_cmd, BaseFlagSet, "read-channel [options] <channel_name>", "Read the content of a channel"},
+	{"reboot", reboot_cmd, BaseFlagSet, "reboot [options] <node_id>", "Reboot node"},
+	{"upload", upload_cmd, BaseFlagSet, "upload [options] <filename> <node_id>", "Upload firmware (intel hex file) to node"},
+}
+
+func FindCommandMatches(cmd string) []*CommandDescriptor {
+	var matches []*CommandDescriptor
+
+	for _, c := range Commands {
+		if strings.HasPrefix(c.command, cmd) {
+			matches = append(matches, c)
+		} else {
+		}
+	}
+	return matches
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "# %s: Missing command\n", os.Args[0])
-		help(os.Stderr)
+		fmt.Fprintf(os.Stderr, "# type `%s help` for usage", os.Args[0])
 		os.Exit(-2)
 	}
 
@@ -530,35 +585,31 @@ func main() {
 		os.Exit(-2)
 	}
 
-	switch os.Args[1] {
-	case "monitor":
-		err = monitor_cmd(os.Args[2:])
-	case "publish":
-		err = publish_cmd(os.Args[2:])
-	case "list-channels":
-		err = list_channels_cmd(os.Args[2:])
-	case "read-channel":
-		err = read_channel_cmd(os.Args[2:])
-	case "list-nodes":
-		err = list_nodes_cmd(os.Args[2:])
-	case "download":
-		err = download_cmd(os.Args[2:])
-	case "upload":
-		err = upload_cmd(os.Args[2:])
-	case "reboot":
-		err = reboot_cmd(os.Args[2:])
-	case "blynk":
-		err = blynk_cmd(os.Args[2:])
-	case "help":
-		err = nil
-		help(os.Stdout)
-	default:
-		fmt.Fprintf(os.Stderr, "%s: Unrecognized command '%s'\n", os.Args[0], os.Args[1])
-		help(os.Stderr)
+	command := FindCommandMatches(os.Args[1])
+	if len(command) == 0 {
+		fmt.Fprintf(os.Stderr, "# Unknown command '%s', type '%s help' for a list of valid commands", os.Args[1], os.Args[0])
 		os.Exit(-2)
 	}
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-		os.Exit(-1)
+	if len(command) > 1 {
+		fmt.Fprintf(os.Stderr, "# Ambiguous command '%s', type '%s help' for a list of valid commands", os.Args[1], os.Args[0])
+		os.Exit(-2)
+	}
+
+	var fs *flag.FlagSet
+
+	fs = command[0].flags(command[0].command)
+	if err = fs.Parse(os.Args[2:]); err != nil {
+		fmt.Fprintf(os.Stderr, "# %s", err)
+		fmt.Fprintf(os.Stderr, "# type '%s help %s' for a list of valid options", os.Args[0], os.Args[1])
+		os.Exit(-2)
+	}
+
+	if command[0].processor != nil {
+		if err = command[0].processor(fs); err != nil {
+			fmt.Fprintf(os.Stderr, "# %s failed: %s", command[0].command, err)
+			os.Exit(-1)
+		}
+	} else {
+		help_cmd(fs)
 	}
 }
