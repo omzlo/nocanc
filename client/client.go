578ec7af1ad64521ca0569c75f4ba13ee4cc2efd
Alain Pannetrat <L1L1@gmx.com>
2019-03-21 10:56:10 +0200

    Getting software update information

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/omzlo/clog"
 	"github.com/omzlo/nocanc/cmd/config"
@@ -46,6 +47,148 @@ func ListNodes() (*socket.NodeList, *Error) {
 		return nil, InternalServerError(err)
 	}
 	return nl, nil
+}
+
+type ArduinoPort struct {
+	Address       string `json:"address"`
+	Label         string `json:"label"`
+	BoardName     string `json:"boardName"`
+	Protocol      string `json:"protocol"`
+	ProtocolLabel string `json:"protocolLabel"`
+	Prefs         struct {
+	} `json:"prefs"`
+	IdentificationPrefs struct {
+	} `json:"identificationPrefs"`
+}
+
+type ArduinoPortDescriptor struct {
+	Port ArduinoPort `json:"port"`
+}
+
+type ArduinoDiscoveryListEvent struct {
+	EventType string                   `json:"eventType"`
+	Ports     []*ArduinoPortDescriptor `json:"ports"`
+}
+
+func ArduinoDiscoverNodes() (string, error) {
+	nl, err := ListNodes()
+	if err != nil {
+		return "", err.GoError()
+	}
+
+	port_list := &ArduinoDiscoveryListEvent{EventType: "list", Ports: make([]*ArduinoPortDescriptor, 0, 8)}
+
+	for _, node := range nl.Nodes {
+		if node.State == models.NodeStateConnected {
+			port := &ArduinoPortDescriptor{
+				Port: ArduinoPort{
+					Address:       fmt.Sprintf("%d", node.Id),
+					Label:         fmt.Sprintf("node %d", node.Id),
+					BoardName:     "Omzlo CANZERO",
+					Protocol:      "nocan",
+					ProtocolLabel: "NoCAN Nodes",
+				},
+			}
+			port_list.Ports = append(port_list.Ports, port)
+		}
+	}
+	r, errx := json.MarshalIndent(port_list, "", "  ")
+	if errx != nil {
+		return "", errx
+	}
+	return string(r), nil
+}
+
+type ArduinoDiscoverySyncEvent struct {
+	EventType string      `json:"eventType"`
+	Port      ArduinoPort `json:"port"`
+}
+
+func arduinoDiscoveryNodeUpdate(node *socket.NodeUpdate) (string, error) {
+
+	if node.State == models.NodeStateConnected || node.State == models.NodeStateUnresponsive {
+		port := &ArduinoDiscoverySyncEvent{
+			Port: ArduinoPort{
+				Address:       fmt.Sprintf("%d", node.Id),
+				Label:         fmt.Sprintf("node %d", node.Id),
+				BoardName:     "Omzlo CANZERO",
+				Protocol:      "nocan",
+				ProtocolLabel: "NoCAN Nodes",
+			},
+		}
+		if node.State == models.NodeStateConnected {
+			port.EventType = "add"
+		} else {
+			port.EventType = "remove"
+		}
+		r, errx := json.MarshalIndent(port, "", "  ")
+		if errx != nil {
+			return "", errx
+		}
+		return string(r), nil
+	}
+	return "", nil
+}
+
+func ArduinoDiscoverNodesSync(print_cb func(string)) error {
+
+	conn, err := config.DialNocanServer()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	sl := socket.NewSubscriptionList(socket.NodeListEvent, socket.NodeUpdateEvent)
+	if err = conn.Subscribe(sl); err != nil {
+		return err
+	}
+
+	if err = conn.Put(socket.NodeListRequestEvent, nil); err != nil {
+		return err
+	}
+
+	value, err := conn.WaitFor(socket.NodeListEvent)
+	if err != nil {
+		return err
+	}
+	nl := new(socket.NodeList)
+	if err = nl.UnpackValue(value); err != nil {
+		return err
+	}
+
+	for _, node := range nl.Nodes {
+		s, err := arduinoDiscoveryNodeUpdate(node)
+		if err != nil {
+			return err
+		}
+		print_cb(s)
+	}
+
+	for {
+		nval, err := conn.WaitFor(socket.NodeUpdateEvent)
+		if err != nil {
+			return err
+		}
+		nu := new(socket.NodeUpdate)
+		if err = nu.UnpackValue(nval); err != nil {
+			return err
+		}
+		s, err := arduinoDiscoveryNodeUpdate(nu)
+		if err != nil {
+			return err
+		}
+		print_cb(s)
+	}
+}
+
+type ArduinoDiscoveryErrorEvent struct {
+	EventType string `json:"eventType"`
+	Message   string `json:"message"`
+}
+
+func ArduinoDiscoverError(msg string) string {
+	err := ArduinoDiscoveryErrorEvent{"error", msg}
+	r, _ := json.MarshalIndent(err, "", "  ")
+	return string(r)
 }
 
 func GetNode(nodeId uint) (*socket.NodeUpdate, *Error) {
--- client/omzlo_info.go
+++ client/omzlo_info.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/omzlo/clog"
+	"github.com/omzlo/nocand/models/device"
 	"net/http"
 	"time"
 )
@@ -19,31 +20,42 @@ var LatestNews OmzloNews
 var http_client = &http.Client{Timeout: 10 * time.Second}
 
 func UpdateLatestNews(version string, os string, arch string) {
+	var di *device.Info
 
-	di, cerr := GetDeviceInformation()
-	if cerr != nil {
+	for {
+		di_x, cerr := GetDeviceInformation()
+		if cerr == nil {
+			di = di_x
+			break
+		}
 		clog.Warning("Could not get device information: %s.", cerr.Error)
-		return
+		time.Sleep(1 * time.Hour)
 	}
 
-	url := fmt.Sprintf("https://www.omzlo.com/software_update?i=%s&cv=%s&o=%s&a=%s&c=webui",
-		base64.StdEncoding.EncodeToString(di.ChipId[:]),
-		version,
-		os,
-		arch)
+	start := time.Now()
 
-	r, err := http_client.Get(url)
-	if err != nil {
-		clog.Warning("Could not get latest news from omzlo.com (Set check-for-updates to 'false' in your configuration to dissable check for updates.)")
-		return
-	}
-	defer r.Body.Close()
+	for {
+		url := fmt.Sprintf("https://www.omzlo.com/software_update?i=%s&cv=%s&o=%s&a=%s&c=webui&u=%d",
+			base64.StdEncoding.EncodeToString(di.ChipId[:]),
+			version,
+			os,
+			arch,
+			time.Since(start)/time.Second)
 
-	err = json.NewDecoder(r.Body).Decode(&LatestNews)
-	if err != nil {
-		clog.Warning("Could not decode latest news from omzlo.com in JSON: %s.", err.Error())
-		return
+		r, err := http_client.Get(url)
+		if err != nil {
+			clog.Warning("Could not get latest news from omzlo.com (Set check-for-updates to 'false' in your configuration to dissable check for updates.)")
+		} else {
+			err = json.NewDecoder(r.Body).Decode(&LatestNews)
+			r.Body.Close()
+
+			if err != nil {
+				clog.Warning("Could not decode latest news from omzlo.com in JSON: %s.", err.Error())
+				return
+			}
+			LatestNews.Loaded = true
+			clog.Info("Loaded latest news from omzlo.com")
+		}
+		time.Sleep(24 * time.Hour)
 	}
-	LatestNews.Loaded = true
-	clog.Info("Loaded latest news from omzlo.com")
 }
